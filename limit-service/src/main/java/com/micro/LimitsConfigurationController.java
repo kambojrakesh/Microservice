31b92f6fb02e6c9a2fb8df778a62245ff6d23403
rakesh-kamboj <rakeshkamboj1826@gmail.com>
2020-04-11 01:22:52 +0530

    First commit

---
+++ limit-service/src/main/java/com/micro/LimitsConfigurationController.java
@@ -0,0 +1,31 @@
+package com.micro;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.micro.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public LimitConfiguration retrieveLimitsFromConfigurations() {
+		LimitConfiguration limitConfiguration = new LimitConfiguration(configuration.getMaximum(),
+				configuration.getMinimum());
+		return limitConfiguration;
+	}
+
+	@GetMapping("/fault-tolerance-example")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	}
+
+}
---
+++ limit-service/src/main/java/com/micro/LimitsServiceApplication.java
@@ -0,0 +1,15 @@
+package com.micro;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class LimitsServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+}
